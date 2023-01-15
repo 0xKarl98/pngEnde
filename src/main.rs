6acdd3c01c740446b5b85135e6246976307a2e3c
Karl <karl@hashcloak.com>
2023-01-15 20:52:09 +0800

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ mod chunk_type;
 mod commands;
 mod png;
 
+//dyn is a shorthand for dynamic
+//it can refer to any type that implements the "Error" trait 
 pub type Error = Box<dyn std::error::Error>;
+
+
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    todo!()
+    let cli = args::Cli::from_args();
+    commands::run(cli.subcommand) 
 }
